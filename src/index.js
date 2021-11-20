2fcc39bfd130e0c0199457c797a934bca44bf0f3
Tyler Smith <tylersmithlf@gmail.com>
2021-11-20 13:29:17 -0700

    Mid day updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDoc } from 'firebase/firestore';
+import { getFirestore, collection, doc} from 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = initializeApp ({
+const firebaseApp = initializeApp ({
     apiKey: "AIzaSyBIYW3eemwRq0RRDYHqv8VlCtWM5Xnzn5s",
     authDomain: "sandbox-hackathon-ae746.firebaseapp.com",
     databaseURL: "https://sandbox-hackathon-ae746.Firebaseio.com",
@@ -13,8 +13,24 @@ const firebaseConfig = initializeApp ({
     measurementId: "G-7J6ZK7K552"
   });
 
-const firestore = getFirestore(firebaseConfig);
+const firestore = getFirestore();
 
-const users = collection(firestore, "users"); //retrieving the users on our page
+// create new document
+const patient = doc(firestore, 'patients/patient-1'); //read document)
 
-const user = getDoc(firestore, "users", user)
+function writePatient() {
+  const docData = {
+    fullName: 'Bryson Lichtenberg',
+    age: 24,
+    phone: 9496144828,
+    zipcode: 84606,
+    gender: 'male'
+  };
+  try {
+    await updateDoc(patient, docData, { merge: true });
+    //updateDoc(patient, docData) will only override the data specified
+    console.log("This patient has been submitted to the database.");
+  } catch {
+    console.log("I got an error! ${error}");
+  }
+}
