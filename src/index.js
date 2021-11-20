62e2b366e294fe50ff80d0cbeecbd00f096b2e9e
Tyler Smith <tylersmithlf@gmail.com>
2021-11-20 14:28:26 -0700

    Firestore edits

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, doc} from 'firebase/firestore';
+// import React, { useRef, useState } from "react";
+import "./App.css";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/analytics";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { useCollectionData } from "react-firebase-hooks/firestore";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseApp = initializeApp ({
-    apiKey: "AIzaSyBIYW3eemwRq0RRDYHqv8VlCtWM5Xnzn5s",
-    authDomain: "sandbox-hackathon-ae746.firebaseapp.com",
-    databaseURL: "https://sandbox-hackathon-ae746.Firebaseio.com",
-    projectId: "sandbox-hackathon-ae746",
-    storageBucket: "sandbox-hackathon-ae746.appspot.com",
-    messagingSenderId: "81298706472",
-    appId: "1:81298706472:web:2269aefbd7d810d060651d",
-    measurementId: "G-7J6ZK7K552"
-  });
+firebase.initializeApp({
+  apiKey: "AIzaSyBIYW3eemwRq0RRDYHqv8VlCtWM5Xnzn5s",
+  authDomain: "sandbox-hackathon-ae746.firebaseapp.com",
+  // databaseURL: "https://sandbox-hackathon-ae746.Firebaseio.com",
+  projectId: "sandbox-hackathon-ae746",
+  storageBucket: "sandbox-hackathon-ae746.appspot.com",
+  messagingSenderId: "81298706472",
+  appId: "1:81298706472:web:2269aefbd7d810d060651d",
+  measurementId: "G-7J6ZK7K552"
+});
 
-const firestore = getFirestore();
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
-// create new document
-const patient = doc(firestore, 'patients/patient-1'); //read document)
+const patient = doc(firestore, 'patients/patient-1'); //read document
 
-function writePatient() {
+function addPatient() {
+  debugger
   const docData = {
-    fullName: 'Bryson Lichtenberg',
-    age: 24,
+    fullName: "Bryson Lichtenberg",
+    age: 23,
     phone: 9496144828,
     zipcode: 84606,
     gender: 'male'
@@ -30,7 +37,7 @@ function writePatient() {
     await updateDoc(patient, docData, { merge: true });
     //updateDoc(patient, docData) will only override the data specified
     console.log("This patient has been submitted to the database.");
-  } catch {
-    console.log("I got an error! ${error}");
+  } catch (error) {
+    console.log("Error: could not import");
   }
-}
+};
